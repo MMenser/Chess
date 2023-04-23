a388547185aa19cca6f4dc85232c3f0084e14dc9
MMenser <119845585+MMenser@users.noreply.github.com>
2023-04-23 15:28:15 -0700

    pawn constructor

--- Pawn.h
+++ Pawn.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "Piece.h"
+
+class Pawn : public Piece {
+public:
+	Pawn(int newX, int newY, int newColor) : Piece(newX, newY, newColor) {
+		std::string newMove = (this->x + 2) + "," + this->y;
+		this->moves.push_back(newMove);
+
+		newMove = (this->x + 1) + "," + this->y;
+		this->moves.push_back(newMove);
+	}
+private:
+};
