4d48450ebccc2a363deed7a426b27eeb45cb24cf
MMenser <119845585+MMenser@users.noreply.github.com>
2023-04-23 15:03:41 -0700

    Updating piece

--- Piece.h
+++ Piece.h
@@ -1,1 +1,40 @@
 #pragma once
+
+
+class Piece {
+public:
+	Piece(int newX, int newY, int newColor) {
+
+	}
+
+
+
+
+	void setX(int newX) {
+		this->x = newX;
+	}
+
+	void setY(int newY) {
+		this->y = newY;
+	}
+
+	void setColor(int newColor) {
+		this->color = newColor;
+	}
+
+	int getX() {
+		return this->x;
+	}
+
+	int getY() {
+		return this->y;
+	}
+
+	int getColor() {
+		return this->color;
+	}
+
+private:
+	int x, y, color; 
+
+};
